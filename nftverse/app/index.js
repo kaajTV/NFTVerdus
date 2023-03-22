5a5dae4abb99b96526fbffab9da7325908d21f19
Anton kaaj Holmgren <anton.holmgren3@hotmail.com>
2023-03-22 13:50:46 +0100

    Update

--- nftverse/app/index.js
+++ nftverse/app/index.js
@@ -18,7 +18,7 @@ const styles = {
 
     // Hero Section
     heroHeading: 'font-bold lg:text-[144px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
-    heroDText: 'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] md:border-[18px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+    heroDText: 'md:w-[162px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] sm:border-[12px] border-[8px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
 };
 
 export default styles;
